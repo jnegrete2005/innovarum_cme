02d1e12bb73d27dc7d250f3f37f33a9ffe57e677
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-11-11 16:40:20 -0500

    Add env var to SECRET_KEY

--- innovarum_cme/settings.py
+++ innovarum_cme/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=gr77x3vfv2cv!bnpodt*zy!f3gg7c2)a7=(+d58wc-=06ftpf'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
