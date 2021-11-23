6363ff9f13e965b6e7c918e51810b47bfd61efe5
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-11-23 15:52:22 -0500

    Change project's name
    v1.2.4

--- innovarum/asgi.py
+++ innovarum/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for innovarum_cme project.
+ASGI config for innovarum project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innovarum_cme.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innovarum.settings')
 
 application = get_asgi_application()
