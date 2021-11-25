3e384ce54796ebb76fdb836cebcdb85826221571
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-11-25 13:10:10 -0500

    Start version of models for Legacy

--- legacy/models.py
+++ legacy/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+from ..cme.models import Bussines
+
 # Create your models here.
+
+
+class Course(models.Model):
+  """
+  Represents a Course for Legacy.
+  It's contents will be stored in `Module`s
+  """
+  name = models.CharField(max_length=200, unique=True)
+
+  def __str__(self) -> str:
+    return f'{self.name}'
+
+
+class Module(models.Model):
+  """
+  Represents a Module for a course.
+  """
+  index = models.PositiveSmallIntegerField(unique=True)
+  course = models.ForeignKey(Course, on_delete=models.PROTECT, related_name='modules')
+
+  def __str__(self) -> str:
+    return f'Módulo {self.index}'
+
+
+class LegacyUser(models.Model):
+  """
+  Represents a User for Legacy
+  """
+  base_user = models.OneToOneField(Bussines, on_delete=models.CASCADE, related_name='legacy')
