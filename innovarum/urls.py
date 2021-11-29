a8a63030eec97a4fc81e140789a563e72cd5c4d2
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-11-29 18:48:13 -0500

    Redirect to CME on empty URL

--- innovarum/urls.py
+++ innovarum/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('cme:index', permanent=False)),
     path('admin/', admin.site.urls),
     path('cme/', include('cme.urls')),
     path('pres/', include('presupuestos.urls')),
