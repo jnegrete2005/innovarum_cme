e0a3d4e5953cb79a666fa59647299104f8d2aaf0
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-11-29 18:34:56 -0500

    Finish presp

--- presupuestos/urls.py
+++ presupuestos/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 from . import views
 
 app_name = 'presupuestos'
 urlpatterns = [
-    path('', login_required(views.index), name='index'),
-    path('get/', login_required(views.get_db), name='getDb'),
+    path('', staff_member_required(views.index, login_url='cme:login'), name='index'),
+    path('get/', staff_member_required(views.get_csv, login_url='cme:login'), name='getDb'),
 ]
