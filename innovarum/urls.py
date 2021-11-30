a8b862517f1cfd16ba63f8aa3e917b7dc1954ae8
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-11-30 17:55:47 -0500

    Add media and static urls to the main urls.

--- innovarum/urls.py
+++ innovarum/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
@@ -24,3 +26,9 @@ urlpatterns = [
     path('pres/', include('presupuestos.urls')),
     path('legacy/', include('legacy.urls')),
 ]
+
+if settings.DEBUG:
+  urlpatterns += (
+      static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
+      static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  )
