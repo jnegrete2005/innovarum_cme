0be975aec94cb7766d595a5723b3eb302b7c5428
Joaquin Negrete <joaquin.negrete03@gmail.com>
2022-01-11 15:06:49 -0500

    Fix bug

--- legacy/urls.py
+++ legacy/urls.py
@@ -6,13 +6,13 @@ from . import views
 
 app_name = 'legacy'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', login_required(views.index, login_url='legacy:login'), name='index'),
     # path('quiz/<int:id>/', login_required(views.quiz_view), name='take_quiz'),
 
     # Create URLs
-    path('crear/', staff_member_required(views.create_view), name='create'),
-    path('crear/quiz/', staff_member_required(views.create_quiz), name='create_quiz'),
-    path('crear/curso/', staff_member_required(views.create_course), name='create_course'),
+    path('crear/', staff_member_required(views.create_view, login_url='legacy:login'), name='create'),
+    path('crear/quiz/', staff_member_required(views.create_quiz, login_url='legacy:login'), name='create_quiz'),
+    path('crear/curso/', staff_member_required(views.create_course, login_url='legacy:login'), name='create_course'),
 
     # User URLs
     path('login', views.login_view, name='login'),
